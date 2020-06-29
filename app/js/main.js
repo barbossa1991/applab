09f5dfd1cab7141e24155347aeeebf29bba9802a
Radyslav Dmytrenko <1991barbossa@gmail.com>
2020-06-29 17:29:58 +0300

    map section with slider is complete

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,10 @@ $(function () {
         });
     });
 
+    $('.map-block__slider').slick({
+        infinite: false,
+        prevArrow: '<button type="button" class="slick-btn slick-stick slick-prev"><div class="arrow-prev"></div></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><div class="arrow-next"></div></button>',
+    });
+
 });
